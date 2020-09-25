7ffa93bb2d9ed59fb61a0dc92b0a5c340fbd35fc
OSAMA ANSARI <mohdbilal16@gmail.com>
2020-09-25 11:52:18 +0530

    Updates

---
+++ precache-manifest.d9136ef402213208850a99fc970f1ab6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07206b3d45d1c4e1875fc0c608d6ec3b",
+    "url": "/NL-Task/index.html"
+  },
+  {
+    "revision": "e08affc9160c39ec666f",
+    "url": "/NL-Task/static/js/2.74a1de1c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/NL-Task/static/js/2.74a1de1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99c498412139f84aafd6",
+    "url": "/NL-Task/static/js/main.736466e3.chunk.js"
+  },
+  {
+    "revision": "5e12a826f238413c665f",
+    "url": "/NL-Task/static/js/runtime-main.8a6b5b43.js"
+  }
+]);
